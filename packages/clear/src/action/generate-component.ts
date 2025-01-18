dfd2f2caa947e2e4ac0dec1cb599cb50fb177f03
young <liyang_email@163.com>
2025-01-18 21:56:18 +0800

    feat(clear): add template logic

---
+++ packages/clear/src/action/generate-component.ts
@@ -0,0 +1,32 @@
+import path from "path";
+import { getDirname, renderHbsToFile, transformVariableName } from "../utils";
+
+export default async function generateComponentAction(name: string){
+    const { pascalCase: componentName, kebabCase: fileName } = transformVariableName(name)
+    const fileData = {
+        created: new Date().toISOString().split('T')[0],
+        componentName,
+    }
+    // 当前路径（需要输出文件夹的路径）
+    const outPath = path.join(process.cwd(), fileName);
+    // 组件 template 所在路径
+    const templateDirPath = path.join(getDirname(), "../src", 'template/component');
+
+    const tsxTplPath = path.join(templateDirPath, 'tsx.hbs');
+    const tsxTargetFilePath = path.join(outPath, 'index.tsx');
+
+    const interfaceTplPath = path.join(templateDirPath, 'interface.hbs');
+    const interfaceTargetFilePath = path.join(outPath, 'interface.ts');
+
+    const storeTplPath = path.join(templateDirPath, 'store.hbs');
+    const storeTargetFilePath = path.join(outPath, 'store.ts');
+
+    try {
+        await renderHbsToFile(tsxTplPath, tsxTargetFilePath, fileData)
+        await renderHbsToFile(interfaceTplPath, interfaceTargetFilePath, fileData)
+        await renderHbsToFile(storeTplPath, storeTargetFilePath, fileData)
+        console.log(`React ${fileName} 模版文件已生成成功！`)
+    } catch (error: any){
+        console.error(error)
+    }
+}
---
+++ packages/clear/src/action/index.ts
@@ -0,0 +1,3 @@
+export { default as generateComponentAction  } from "./generate-component"
+export { default as generateStoreAction  } from "./generate-store"
+export { default as generateServiceAction  } from "./generate-service"
